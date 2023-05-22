ac029a01dc6f6afa684be458cc8fbc0a3456dbde
mrpandemonium <wincentsouriya@gmail.com>
2023-05-22 13:02:36 -0700

    imported materialize

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [displayTime, setDisplayTime] = useState(25 * 60);
+
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (
+      (minutes < 10 ? "0" + minutes : minutes) +
+      ":" +
+      (seconds < 10 ? "0" + seconds : seconds)
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Length
+        title={"break length"}
+        changeTime={null}
+        type={"break"}
+        time={null}
+        formatTime={formatTime}
+      />
+      <div id="break-label">{formatTime(displayTime)}</div>
+    </div>
+  );
+}
+
+function Length({ title, changeTime, type, time, formatTime }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div class="time-sets">
+        <button class="btn-small deep-purple lighten-2">
+          <i class="material-icons">arrow_downward</i>
+        </button>
+      </div>
     </div>
   );
 }
