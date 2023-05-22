0fb7c56f7446ecd2e9f299086fbcd29556133afe
mrpandemonium <wincentsouriya@gmail.com>
2023-05-22 14:38:44 -0700

    added mp3 file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import "./App.css";
 
 function App() {
   const [displayTime, setDisplayTime] = useState(25 * 60);
+  const [breakTime, setBreakTime] = useState(5 * 60);
+  const [sessionTime, setSessionTime] = useState(25 * 60);
+  const [timerOn, setTimerOn] = useState(false);
+  const [onBreak, setOnBreak] = useState(false);
+  const [breakAudio, setBreakAudio] = useState(new Audio("./breakTime.mp3"));
+
+  const playBreakSound = () => {
+    breakAudio.currentTime = 0;
+    breakAudio.play();
+  };
 
   const formatTime = (time) => {
     let minutes = Math.floor(time / 60);
@@ -14,16 +24,98 @@ function App() {
     );
   };
 
+  const changeTime = (amount, type) => {
+    if (type == "break") {
+      if (breakTime <= 60 && amount < 0) {
+        return;
+      }
+      setBreakTime((prev) => prev + amount);
+    } else {
+      if (sessionTime <= 60 && amount < 0) {
+        return;
+      }
+      setSessionTime((prev) => prev + amount);
+      if (!timerOn) {
+        setSessionTime(sessionTime + amount);
+      }
+    }
+  };
+
+  const controlTime = () => {
+    let second = 1000;
+    let date = new Date().getTime();
+    let nextDate = new Date().getTime() + second;
+    let onBreakVariable = onBreak;
+    if (!timerOn) {
+      let interval = setInterval(() => {
+        date = new Date().getTime();
+        if (date > nextDate) {
+          setDisplayTime((prev) => {
+            if (prev <= 0 && !onBreakVariable) {
+              playBreakSound();
+              onBreakVariable = true;
+              setOnBreak(true);
+              return breakTime;
+            } else if (prev <= 0 && onBreakVariable) {
+              playBreakSound();
+              onBreakVariable = false;
+              setOnBreak(false);
+              return sessionTime;
+            }
+            return prev - 1;
+          });
+          nextDate += second;
+        }
+      }, 30);
+      localStorage.clear();
+      localStorage.setItem("interval-id", interval);
+    }
+    if (timerOn) {
+      clearInterval(localStorage.getItem("interval-id"));
+    }
+    setTimerOn(!timerOn);
+  };
+
+  const resetTime = () => {
+    setDisplayTime(25 * 60);
+    setBreakTime(5 * 60);
+    setSessionTime(25 * 60);
+  };
+
   return (
-    <div className="App">
-      <Length
-        title={"break length"}
-        changeTime={null}
-        type={"break"}
-        time={null}
-        formatTime={formatTime}
-      />
-      <div id="break-label">{formatTime(displayTime)}</div>
+    <div className="center-align">
+      <h1>Clock</h1>
+      <div className="dual-container">
+        <Length
+          title={"break length"}
+          changeTime={changeTime}
+          type={"break"}
+          time={breakTime}
+          formatTime={formatTime}
+        />
+        <Length
+          title={"session length"}
+          changeTime={changeTime}
+          type={"session"}
+          time={sessionTime}
+          formatTime={formatTime}
+        />
+        <h3>{onBreak ? "Break" : "Session"}</h3>
+        <h1>{formatTime(displayTime)}</h1>
+        <button
+          className="btn-large deep-purple lighten-2"
+          onClick={controlTime}
+        >
+          {timerOn ? (
+            <i className="material-icons">pause_circle_filled</i>
+          ) : (
+            <i className="material-icons">play_circle_filled</i>
+          )}
+        </button>
+        <button className="btn-large-deep purple lighten-2" onClick={resetTime}>
+          <i className="material-icons">autorenew</i>
+        </button>
+      </div>
     </div>
   );
 }
@@ -32,9 +124,19 @@ function Length({ title, changeTime, type, time, formatTime }) {
   return (
     <div>
       <h3>{title}</h3>
-      <div class="time-sets">
-        <button class="btn-small deep-purple lighten-2">
-          <i class="material-icons">arrow_downward</i>
+      <div className="time-sets">
+        <button
+          className="btn-small deep-purple lighten-2"
+          onClick={() => changeTime(-60, type)}
+        >
+          <i className="material-icons">arrow_downward</i>
+        </button>
+        <h3>{formatTime(time)}</h3>
+        <button
+          className="btn-small deep-purple lighten-2"
+          onClick={() => changeTime(+60, type)}
+        >
+          <i className="material-icons">arrow_upward</i>
         </button>
       </div>
     </div>
